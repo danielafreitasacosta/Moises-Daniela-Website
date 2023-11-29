04abcc3d618d9275082146dd882ce70e66089ea1
perezmm75 <113702980+perezmm75@users.noreply.github.com>
2023-11-29 21:50:56 +0100

    hr para la linea blanca
    Estoy probando si con el hr es más fácil crear una linea blanca de 2px

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,12 +2,14 @@ import "./Home.css";
 import React from 'react';
 import NavigationBar from "src/components/core/NavigationBar";
 import 'src/pages/home/Home' // Crearemos este archivo en el siguiente paso
+import './App.css';
 
 const Home = () => {
   return (
     <div className="image-grid">
        <NavigationBar/>
       <p>Hola me llamo Moisés</p>
+      <hr style={{ width: '50%', backgroundColor: 'white', height: '2px', border: 'none' }} />
      
     </div>
   );
