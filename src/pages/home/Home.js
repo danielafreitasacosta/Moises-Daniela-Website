3e47293765541eb3bd4c278c31f3f730e40a0d65
perezmm75 <113702980+perezmm75@users.noreply.github.com>
2023-11-30 20:02:31 +0100

    pequeños cambios
    Retoque de mejoras como la dirección de la imagen de fondo

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,15 +2,18 @@ import "./Home.css";
 import React from 'react';
 import NavigationBar from "src/components/core/NavigationBar";
 import 'src/pages/home/Home' // Crearemos este archivo en el siguiente paso
-import './App.css';
+import InfoTop from "../info-top/InfoTop";
 
 const Home = () => {
   return (
     <div className="image-grid">
-       <NavigationBar/>
-      <p>Hola me llamo Moisés</p>
-      <hr style={{ width: '50%', backgroundColor: 'white', height: '2px', border: 'none' }} />
-     
+      <div className="paragraf1">
+        <InfoTop />
+      </div>
+
+      <hr style={{ width: '100%', backgroundColor: 'white', height: '2px', border: 'none', marginTop: '40px' }} />
+      <NavigationBar />
+
     </div>
   );
 };
---
+++ src/pages/info-top/InfoTop.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+
+export default function InfoTop() {
+    return (
+        <div>
+            <p>
+               Wine Tastings and winery tours
+            </p>
+        </div>
+    )
+}
