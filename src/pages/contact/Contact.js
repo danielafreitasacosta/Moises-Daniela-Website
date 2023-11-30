80da58918f3fc8aa509941ff0cfd3d9bd19dd9c6
Daniela Freitas <danielafreitasacosta@gmail.com>
2023-11-30 19:51:11 +0100

    Creada carpeta de estilos y fichero .js
    Creada carpeta de estilos y fichero .js con tipografía y colores del tema.
    Falta: modificar la tipografía por la que nos piden en el práctica.

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -3,6 +3,10 @@ import React, { useRef } from 'react';
 import NavigationBar from "src/components/core/NavigationBar";
 import 'src/pages/contact/Contact'
 import emailjs from '@emailjs/browser';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import 'src/styles/globalStyles.css'; 
+import defaultTheme from "src/styles/themes/defaultTheme";
 
 /*
 Credenciales de la cuenta de Gmail donde llegan los correos: 
@@ -35,6 +39,7 @@ const Contact = () => {
 
     return (
         <>
+        <div style={{ color: defaultTheme.colors.primary}}> 
             <div className="image-grid-contact">
                 <div className="img-center">
                     <NavigationBar />
@@ -45,14 +50,16 @@ const Contact = () => {
                     <h2 >Contacto</h2>
 
                     <form ref={form} onSubmit={sendEmail} className="column" >
-                        <input type="text" placeholder="Full Name" name="user_name" required></input>
+                    <TextField id="outlined-basic" variant="outlined" type="text" placeholder="Full Name" name="user_name" required/>
+                       
                         <input type="email" placeholder="Email" name="user_email" required></input>
                         <input type="text" placeholder="Subject" name="subject" required></input>
                         <textarea name="message" cols={30} rows={10}></textarea>
-                        <button type="submit"> Send Message </button>
+                        <Button variant="contained" type="submit"> Send Message </Button>
                     </form>
                 </div>
             </section>
+            </div>
         </>
     );
 };
