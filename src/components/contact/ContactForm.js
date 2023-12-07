8702619c07b944f776caa9a8d65746dd3b2f3485
Daniela Freitas <danielafreitasacosta@gmail.com>
2023-12-07 19:57:31 +0100

    finished contact page
    finished contact page

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import {Button, TextField } from '@mui/material'; 
 import {Controller, useForm} from 'react-hook-form'; 
 import 'src/services/theme'; 
@@ -5,12 +6,65 @@ import 'src/services/theme';
 
 function ContactForm() {
     const { handleSubmit, control, formState: {errors}, reset} = useForm()
+=======
+import { Button, TextField } from '@mui/material';
+import { Controller, useForm } from 'react-hook-form';
+import Stack from '@mui/material/Stack';
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+
+/*
+Credenciales de la cuenta de Gmail donde llegan los correos: 
+Correo: cellertecnocampus@gmail.com
+Contraseña: CELLerTECNOcampus2023!
+*/
+
+function SendEmail() {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(
+            'service_pddibau',
+            'template_1qz768n',
+            form.current,
+            'hsXoj_9R8eIZQIEa5'
+        )
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset();
+    };
+}
+
+function ContactForm() {
+    const formStyle = {
+        marginBottom: "8px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center", 
+		
+    };
+
+    const { control, handleSubmit, formState: { errors }, reset } = useForm({
+        defaultValues: {
+            fieldName: ''
+        }
+    });
+>>>>>>> Stashed changes
 
     function onSubmit (value) {
         //Enviar el email al servidor 
         console.log(value); 
         reset()
     }
+
+   
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}> 
@@ -32,10 +86,82 @@ function ContactForm() {
                     />     
             }
 
+<<<<<<< Updated upstream
         />
             <Button variant='contained' type='submit' >Send</Button>
         </form>
 
+=======
+            <form style={formStyle} onSubmit={handleSubmit(SendEmail)}>
+
+              <Stack spacing={2}>  
+
+                <Controller
+                    name="fieldName"
+                    control={control}
+                    rules={{ required: 'Este campo es obligatorio' }}
+                    render={({ field }) =>
+                        <TextField
+                            {...field}
+                            label="Nombre"
+                            variant="outlined"
+                            size='small'
+                            error={!!errors.fieldName}
+                            helperText={errors?.fieldName?.message.toString()}
+                        />
+                    }
+
+
+                />
+
+                <Controller
+                    name="fieldName"
+                    control={control}
+                    rules={{ required: 'Este campo es obligatorio' }}
+                    render={({ field }) =>
+                        <TextField
+                            {...field}
+                            label="Email"
+                            variant="outlined"
+                            size='small'
+                            error={!!errors.fieldName}
+                            helperText={errors?.fieldName?.message.toString()}
+                        />
+                    }
+
+
+                />
+
+                <Controller
+                    name="fieldName"
+                    control={control}
+                    rules={{ required: 'Debes rellenar este campo' }}
+                    render={({ field }) =>
+                        <TextField
+                            {...field}
+                            label="Mensaje"
+                            multiline
+                            rows={4}
+                            error={!!errors.fieldName}
+                            helperText={errors?.fieldName?.message.toString()
+                            }
+                        />
+                    }
+                />
+
+
+                <Button variant="contained" type="submit" color="primary" onClick={SendEmail}>
+                    Enviar
+                </Button>
+
+                </Stack>
+
+            </form>
+
+
+
+        </>
+>>>>>>> Stashed changes
     )
 
 }
--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,4 +1,5 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import "./Contact.css";
 import React, { useRef } from 'react';
 import NavigationBar from "src/components/core/NavigationBar";
@@ -22,10 +23,16 @@ import ContactForm from 'src/components/contact/ContactForm';
 import { ThemeProvider} from '@mui/material/styles'
 import { green } from '@mui/material/colors';
 import 'src/services/theme'; 
+=======
+import { Container, Grid } from "@mui/material";
+import ContactForm from "src/components/contact/ContactForm";
+import NavigationBar from "src/components/core/NavigationBar";
+>>>>>>> Stashed changes
 
 
 <<<<<<< Updated upstream
 const Contact = () => {
+<<<<<<< Updated upstream
     const form = useRef(); 
 
     const sendEmail = (e) => {
@@ -73,6 +80,39 @@ function Contact () {
                 </div>
             </section>
             </div>
+=======
+
+	const h1Style = {
+        marginBottom: "2px",
+        paddingTop: "5%",
+        display: "flex",
+        alignItems: "center",
+		fontFamily: "Montserrat",
+		color: "black",
+    };
+
+
+    return (
+        <>
+		<NavigationBar />
+        <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d47723.691081227495!2d0.5787351987555143!3d41.61833494518562!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a6e048e73bd37f%3A0xa0d32ea2d259aaaf!2zTMOpcmlkYQ!5e0!3m2!1ses!2ses!4v1701962386255!5m2!1ses!2ses"
+        width="100%"
+        height="450"
+        style={{ border: 0}}
+        loading="lazy"
+        />
+
+		<h1 style={h1Style}>
+            Contacto
+		</h1>
+        
+		<Container>
+            <ContactForm />
+        </Container>
+      
+
+>>>>>>> Stashed changes
         </>
     );
 };
