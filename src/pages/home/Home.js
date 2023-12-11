c589274645e636e07ef7a28f15c679497c651b87
Daniela Freitas <danielafreitasacosta@gmail.com>
2023-12-11 02:51:24 +0100

    actualizacion tema light dark

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -8,33 +8,35 @@ import PackFotos from "src/components/packfotos/PackFotos";
 import Weather from "src/components/weather/Weather";
 import Infoenlaces from "src/components/infoenlaces/Infoenlaces";
 import { withTranslation } from "react-i18next"
+import { Paper } from '@mui/material';
 
 const Home = ({ t, i18n }) => {
   return (
     <>
-      <div className="image-grid">
-        <div className="paragraf1">
-          <InfoTop />
+      <Paper>
+        <div className="image-grid">
+          <div className="paragraf1">
+            <InfoTop />
+          </div>
+          <hr style={{ width: '100%', backgroundColor: 'white', height: '2px', border: 'none', marginTop: '20px' }} />
+          <NavigationBar />
+          <h1>{t('present.titulo')}</h1>
+          <p>{t('present.subtitulo')}</p>
+        </div >
+        <div>
+          <Vinya />
         </div>
-        <hr style={{ width: '100%', backgroundColor: 'white', height: '2px', border: 'none', marginTop: '20px' }} />
-        <NavigationBar />
-        <h1>{t('present.titulo')}</h1>
-        <p>{t('present.subtitulo')}</p>
-      </div >
-      <div>
-        <Vinya />
-      </div>
-      <div>
-        <PackFotos />
-      </div>
-      <div>
-        <Weather />
-      </div>
-      <div>
-        <Infoenlaces />
-      </div>
-      
+        <div>
+          <PackFotos />
+        </div>
+        <div>
+          <Weather />
+        </div>
+        <div>
+          <Infoenlaces />
+        </div>
 
+      </Paper>
     </>
   );
 };
--- src/services/theme.js
+++ src/services/theme.js
@@ -1,49 +1,67 @@
 
+import { ListItem } from '@mui/material';
 import { green, grey, pink } from '@mui/material/colors';
 import {
     Experimental_CssVarsProvider as CssVarsProvider,
     experimental_extendTheme as extendTheme,
-  } from '@mui/material/styles';
+} from '@mui/material/styles';
+import { NavLink } from 'react-router-dom';
 
-  const theme = extendTheme({
+
+
+const theme = extendTheme({
     colorSchemes: {
         light: {
             palette: {
                 primary: {
                     main: green[600],
                 },
+                secondary: {
+                    main: green[200],
+                }
             },
+            /*:action: {
+                active: green[200],
+                activeOpacity: 1,
+                hover: green[100],
+                hoverOpacity: 0.7,
+                focus: green[600],
+                focusOpacity: 1,
+                selected: green[300],
+                selectedOpacity: 1
+              },
+              */
         },
 
         dark: {
             palette: {
                 primary: {
                     main: pink[400],
-                }, 
+                },
                 secondary: {
-                    main: '#000'
+                    main: '#fff'
                 },
                 background: {
                     default: pink[900],
-                    paper: pink[200],
+                    paper: grey[600],
                 },
                 text: {
-                    primary: '#000', 
-                    secondary: grey[700], 
+                    primary: grey[50],
+                    secondary: grey[700],
                 },
-            }, 
-        }, 
-    }, 
+            },
+        },
+    },
     typography: {
-        fontFamily: 'Montserrat', 
+        fontFamily: 'Montserrat',
         h3: {
-            fontWeight: '700', 
-        }, 
+            fontWeight: '700',
+        },
         h4: {
-            fontWeight: '700', 
+            fontWeight: '700',
             textTransform: 'uppercase'
         },
     },
-  }); 
+});
 
-  export default theme; 
+export default theme; 
